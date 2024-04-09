cdc23e467f8aeae3100b1a3e018bcc475b39f293
S R Rayhan <shafikulrahman66@gmail.com>
2024-04-09 11:19:30 +0600

    Add sorting and filtering features in tournaments page

--- src/pages/Tournament.jsx
+++ src/pages/Tournament.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../css/tournament.css'
-import { Button, Table, Card } from 'antd';
+import { Button, Table, Card, Tag } from 'antd';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SideMenu from '../components/SideMenu';
 
 import { IoCaretBackOutline } from "react-icons/io5";
+import { render } from 'react-dom';
 
 const Tournament = () => {
     const columns = [
@@ -27,18 +28,42 @@ const Tournament = () => {
             title:'Start Date',
             dataIndex:'startDate',
             key:'startDate',
-            align: 'center'
+            align: 'center',
+            sorter: (a,b)=>new Date(a.startDate) - new Date(b.startDate),
+            sortDirections: ['descend']
         },
         {
             title:'End Date',
             dataIndex:'endDate',
             key:'endDate',
-            align: 'center'
+            align: 'center',
+            sorter: (a,b)=>new Date(a.endDate) - new Date(b.endDate),
+            sortDirections: ['descend']
+        },
+        {
+            title:'Status',
+            key:'status',
+            align:'center',
+            filters: [
+                {text:'Running',value:'Running'},
+                {text:'Finished',value:'Finished'},
+            ],
+            onFilter:(value,record)=>{
+                const currentDate = new Date();
+                const endDate = new Date(record.endDate);
+                return value === 'Running' ? currentDate <= endDate : currentDate > endDate;
+            },
+            render: (_,records)=>{
+                const currentDate = new Date();
+                const endDate = new Date(records.endDate);
+                return (currentDate>endDate ? <Tag color='#87d068'>Finished</Tag> : <Tag color='#108ee9'>Running</Tag>) 
+            }
         },
         {
             title:'Action',
             key:'action',
             align: 'center',
+            
             render: (text,record)=>{
                 return <Button className='see-details-button' onClick={()=>{handleButtonClicked(record)}} type='primary'>See Details</Button>
             }
@@ -56,80 +81,17 @@ const Tournament = () => {
         {
             key:'2',
             serialNo:'2',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
+            tournamentName:'Intra SUST Football Tournament-2023',
+            startDate:'2023-01-12',
+            endDate:'2023-06-12'
         },
         {
             key:'3',
             serialNo:'3',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'1',
-            serialNo:'1',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'2',
-            serialNo:'2',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'3',
-            serialNo:'3',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'1',
-            serialNo:'1',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'2',
-            serialNo:'2',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'3',
-            serialNo:'3',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'1',
-            serialNo:'1',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'2',
-            serialNo:'2',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
-        {
-            key:'3',
-            serialNo:'3',
-            tournamentName:'Intra SUST Football Tournament-2022',
-            startDate:'2022-01-12',
-            endDate:'2022-02-12'
-        },
+            tournamentName:'Intra SUST Football Tournament-2024',
+            startDate:'2024-01-12',
+            endDate:'2024-06-12'
+        }
     ]
 
     const handleButtonClicked = (records) =>{
