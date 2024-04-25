3257fb38fd8ba23c06976f39a60ad9646552cec5
S R Rayhan <shafikulrahman66@gmail.com>
2024-04-25 09:42:05 +0600

    Created APIs in tournament table

--- Backend/routerHandler/tournamentsHandler.js
+++ Backend/routerHandler/tournamentsHandler.js
@@ -34,4 +34,61 @@ router.post('/addTournament',checkLogin,async(req,res)=>{
 });
 
 
+router.get('/allTournament',checkLogin,async(req,res)=>{
+    try{
+        const sql = "SELECT * FROM tournament_tbl";
+        db.query(sql,(err,result)=>{
+            if(err) return res.status(500).json("Failed to get all tournament");
+            return res.status(200).json({
+                message : "Successfully get all tournament",
+                data : result
+            })
+        })
+    }catch(error){
+        console.log(error);
+        res.status(500).json('Error occured to get all tournament');
+    }
+})
+
+
+router.put('/updateATournament',checkLogin,async(req,res)=>{
+    try{
+        const sql = "UPDATE tournament_tbl SET startDate=?,endDate=? WHERE tournamentId=?";
+        const values = [
+            req.body.startDate,
+            req.body.endDate,
+            req.body.tournamentId
+        ]
+        db.query(sql,values,(err,result)=>{
+            if(err) return res.status(500).json('Error occured to update');
+            return res.status(200).json({
+                message : 'Successfully updated',
+                data : result
+            })
+        })
+    }catch(error){
+        console.log(error);
+        res.status(500).json('Error occured while updating')
+    }
+})
+
+
+router.delete('/deleteATournament',checkLogin,async(req,res)=>{
+    try{
+        const sql = "DELETE FROM tournament_tbl WHERE tournamentId=?";
+        const values = [req.body.tournamentId];
+
+        db.query(sql,values,(err,result)=>{
+            if(err) return res.status(500).json('Error occured while deleting');
+            return res.status(200).json({
+                message : "Successfully deleted",
+                data : result
+            })
+        })
+    }catch(error){
+        console.log(error);
+    }
+})
+
+
 module.exports = router;
